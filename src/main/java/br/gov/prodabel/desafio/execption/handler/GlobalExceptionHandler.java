62ef1163ac82b19e9def8b76526d5c26e57fd772
gmozart <mozart.gleison@gmail.com>
2025-09-30 19:36:49 -0300

    Create Configuration for OpenApi and Exceptions messages

---
+++ src/main/java/br/gov/prodabel/desafio/execption/handler/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package br.gov.prodabel.desafio.execption.handler;
+
+import br.gov.prodabel.desafio.execption.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    static class ErrorResponse {
+        private int status;
+        private String message;
+    }
+}
